f939195186a04f762a78e0a3f3bc504bd9ad3b34
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-08-31 14:54:14 -0300

    Entrega requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,13 +8,15 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  let splitPhrase = [];
+  
 }
 
 // Desafio 4
