46ef61d03f825e2b58c31598c71f237345d3b80e
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-01 23:43:29 -0300

    Finaliza desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,10 @@
 // Desafio 1
 function compareTrue(value1, value2) {
+  let result = false;
   if (value1 === true && value2 === true) {
-    return true;
-  } else {
-    return false;
+    result = true;
   }
+  return result;
 }
 
 // Desafio 2
@@ -21,7 +21,8 @@ function splitSentence(phrase) {
 
 // Desafio 4
 function concatName(stringArray) {
-  return stringArray[stringArray.length - 1] + ', ' + stringArray[0];
+  let array = `${stringArray[stringArray.length - 1]}, ${stringArray[0]}`;
+  return array;
 }
 
 // Desafio 5
@@ -49,45 +50,59 @@ function highestCount(array) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
+  let result = '';
   let posCat1ToMouse = Math.abs(cat1 - mouse);
   let posCat2ToMouse = Math.abs(cat2 - mouse);
   if (posCat2ToMouse < posCat1ToMouse) {
-    return 'cat2'
+    result = 'cat2';
   } else if (posCat2ToMouse > posCat1ToMouse) {
-    return 'cat1';
+    result = 'cat1';
   } else {
-    return 'os gatos trombam e o rato foge';
+    result = 'os gatos trombam e o rato foge';
   }
+  return result;
 }
-
 
 // Desafio 8
 function fizzBuzz(array) {
-  // /3 fizz - /5 buzz - /3/5 fizzbuzz - bug
   let stringResult = [];
   for (let item of array) {
     if (item % 3 === 0 && item % 5 === 0) {
-      stringResult.push('fizzBuzz')
+      stringResult.push('fizzBuzz');
     } else if (item % 3 === 0) {
-      stringResult.push('fizz')
+      stringResult.push('fizz');
     } else if (item % 5 === 0) {
-      stringResult.push('buzz')
+      stringResult.push('buzz');
     } else {
-      stringResult.push('bug!')
+      stringResult.push('bug!');
     }
   }
-  return stringResult
+  return stringResult;
 }
 
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(code) {
+  let result = code.replace(/e/g, '2');
+  result = result.replace(/a/g, '1');
+  result = result.replace(/i/g, '3');
+  result = result.replace(/o/g, '4');
+  result = result.replace(/u/g, '5');
+
+  return result;
 }
-function decode() {
-  // seu código aqui
+function decode(code) {
+  let result = code.replace(/2/g, 'e');
+  result = result.replace(/1/g, 'a');
+  result = result.replace(/3/g, 'i');
+  result = result.replace(/4/g, 'o');
+  result = result.replace(/5/g, 'u');
+
+  return result;
 }
+console.log(encode('Hi there'));
+console.log(decode('Hi there'));
 
 module.exports = {
   calcArea,
