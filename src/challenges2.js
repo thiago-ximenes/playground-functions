404a4bbb6f860bad138ae2a140d6771c1c4b304b
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-05 23:12:09 -0300

    Tudo resolvido, faltando refatorar para ficar de acordo com o linter no requisito 8, 11, 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,45 +19,80 @@ function techList(technologies, studentName) {
 function generatePhoneNumber(phoneNumber) {
   let number = phoneNumber;
   if (number.length != 11) {
-      result = 'Array com tamanho incorreto.';
-    } else {
-  let repeat = 0;
-  isPhoneNumber = true;
-  result = '';
-  for (let index = 0; index < number.length; index += 1) {
-    repeat = 0;
-    for (let index2 = 0; index2 < number.length; index2 += 1) {
-      if (number[index2] === number[index]) {
-        repeat += 1;
-        if (repeat >= 3 || number[index] < 0 || number[index] > 9) {
-          isPhoneNumber = false;
-          break;
+    result = 'Array com tamanho incorreto.';
+  } else {
+    let repeat = 0;
+    isPhoneNumber = true;
+    result = '';
+    for (let index = 0; index < number.length; index += 1) {
+      repeat = 0;
+      for (let index2 = 0; index2 < number.length; index2 += 1) {
+        if (number[index2] === number[index]) {
+          repeat += 1;
+          if (repeat >= 3 || number[index] < 0 || number[index] > 9) {
+            isPhoneNumber = false;
+            break;
+          }
         }
       }
     }
+    if (isPhoneNumber) {
+      number = number.join('');
+      number = number.toString();
+      result = number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    } else {
+      result = 'não é possível gerar um número de telefone com esses valores';
+    }
   }
-  if (isPhoneNumber) {
-    number = number.join('');
-    number = number.toString();
-    result = number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
-  } else {
-    result = 'não é possível gerar um número de telefone com esses valores'
-  }
-}
   return result;
 }
 
 // console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(sideA, sideB, sideC) {
+  let a = false;
+  let b = false;
+  let c = false;
+  if (sideA < sideB + sideC && sideA > Math.abs(sideB - sideC)) {
+    a = true;
+  }
+  if (sideB < sideA + sideC && sideB > Math.abs(sideA - sideC)) {
+    b = true;
+  }
+  if (sideC < sideB + sideA && sideA > Math.abs(sideB - sideA)) {
+    c = true;
+  }
+  if (a && b && c) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(triangleCheck(10, 13, 2));
+
+// Desafio 13
+function hydrate(drinks) {
+  let waterCups = drinks.match(/\d+/g);
+  // //https://www.codegrepper.com/code-examples/javascript/get+only+numbers+from+string+js -- usei para saber qual regex usar
+  let result = `${waterCups} copos de água`;
+  if (waterCups.length === 1) {
+    result = `${waterCups} copo de água`;
+  } else {
+    let counter = 0;
+    for (let item of waterCups) {
+      let WaterCupsInInteger = parseInt(item, 10);
+      // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt -- usei para entender como funciona a função parseInt
+      counter += WaterCupsInInteger;
+    }
+    result = `${counter} copos de água`;
+  }
+  return result;
   // seu código aqui
 }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
