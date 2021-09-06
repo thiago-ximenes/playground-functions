cf7d6f69d7a2d8210c5fc581a95e5ff188ae8410
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-05 22:10:05 -0300

    Resolvido exercício 11, porém o linter reclamou de complexidade acima de 5 número de linhas maior que 20

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,15 +18,36 @@ function techList(technologies, studentName) {
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
   let number = phoneNumber;
-  if (number.length === 11) {
-    number = +number.join('');
-    number = number.toString();
-    return number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  if (number.length != 11) {
+      result = 'Array com tamanho incorreto.';
+    } else {
+  let repeat = 0;
+  isPhoneNumber = true;
+  result = '';
+  for (let index = 0; index < number.length; index += 1) {
+    repeat = 0;
+    for (let index2 = 0; index2 < number.length; index2 += 1) {
+      if (number[index2] === number[index]) {
+        repeat += 1;
+        if (repeat >= 3 || number[index] < 0 || number[index] > 9) {
+          isPhoneNumber = false;
+          break;
+        }
+      }
+    }
   }
-  return 'não é possível gerar um número de telefone com esses valores';
+  if (isPhoneNumber) {
+    number = number.join('');
+    number = number.toString();
+    result = number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  } else {
+    result = 'não é possível gerar um número de telefone com esses valores'
+  }
+}
+  return result;
 }
 
-console.log(generatePhoneNumber([0, 2, 3, 4, 5, 7, 7, 8, 9, 0, 7]));
+// console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
 function triangleCheck() {
